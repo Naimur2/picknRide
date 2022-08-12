40d8468f8467015e88f4ead52faa00b2d49ea0c2
naimur2 <naimurrahaman248@gmail.com>
2022-08-12 11:55:25 +0600

    updates

--- src/screens/customDrawer/CustomDrawer.tsx
+++ src/screens/customDrawer/CustomDrawer.tsx
@@ -34,6 +34,10 @@ export default function CustomDrawer() {
             onPress: () => {},
         },
         {
+            title: "Cars",
+            onPress: () => {},
+        },
+        {
             title: "Notifications",
             onPress: () => {},
         },
