07c86b92cc0b99c5267c4b7cabf30ea1c2b5f11c
naimur2 <naimurrahaman248@gmail.com>
2022-08-24 16:09:52 +0600

    updates

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ import { ExpoConfig, ConfigContext } from "@expo/config";
 export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config,
     name: "PickNRide",
+    slug: "PickNRide",
     extra: {
         GOOGLE_MAP_KEY: "AIzaSyDS-lAgjgs0rT8FzzB9woxe8sTPPh6v7F4",
     },
